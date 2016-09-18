fdc89c67c51354c0e6f76d6203b90b92bf7be019
JennyShalai <jenny.shalai@gmail.com>
2016-09-18 13:13:39 -0400

    add constraints to MainVC

--- Roman-Numerals/MainViewController.swift
+++ Roman-Numerals/MainViewController.swift
@@ -24,6 +24,18 @@ class MainViewController: UIViewController {
         self.view.removeConstraints(self.view.constraints)
         self.view.translatesAutoresizingMaskIntoConstraints = false
         
+        self.numToRoman.removeConstraints(self.numToRoman.constraints)
+        self.numToRoman.translatesAutoresizingMaskIntoConstraints = false
+        self.numToRoman.centerXAnchor.constraintEqualToAnchor(self.view.centerXAnchor, constant: -100).active = true
+        self.numToRoman.centerYAnchor.constraintEqualToAnchor(self.view.centerYAnchor).active = true
+        self.numToRoman.heightAnchor.constraintEqualToConstant(100).active = true
+        self.numToRoman.widthAnchor.constraintEqualToConstant(100).active = true
         
+        self.romToNumber.removeConstraints(self.romToNumber.constraints)
+        self.romToNumber.translatesAutoresizingMaskIntoConstraints = false
+        self.romToNumber.centerXAnchor.constraintEqualToAnchor(self.view.centerXAnchor, constant: 100).active = true
+        self.romToNumber.centerYAnchor.constraintEqualToAnchor(self.view.centerYAnchor).active = true
+        self.romToNumber.heightAnchor.constraintEqualToConstant(100).active = true
+        self.romToNumber.widthAnchor.constraintEqualToConstant(100).active = true
     }
 }
